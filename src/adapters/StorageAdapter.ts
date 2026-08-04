516bb061d7f3904d72274d3a06516002ffddce47
timurenk0 <zheltenkovtimur@gmail.com>
2026-08-04 11:17:22 +0200

    Update StorageAdapter interface according to the MemoryAdapter functionality

--- src/adapters/StorageAdapter.ts
+++ src/adapters/StorageAdapter.ts
@@ -1,6 +1,12 @@
-import type { Permission, Role } from "../types.js";
+import type { Permission, Role, RolePermission, User, UserRole } from "../types.js";
 
 export interface StorageAdapter {
+    createUser: (user: User) => void;
+    createRole: (role: Role) => void;
+    createPermission: (permission: Permission) => void;
+    createUserRole: (userRole: UserRole) => void;
+    createRolePermission: (rolePermission: RolePermission) => void;
+
     getUserRoles: (userId: string) => Role[];
     getRolePermissions: (roleId: string) => Permission[];
 }
