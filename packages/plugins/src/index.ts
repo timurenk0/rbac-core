1360394cc179a4e5a63cbf32ea0f4c79181c46a4
timurenk0 <zheltenkovtimur@gmail.com>
2026-09-24 02:29:11 +0200

    Added public boundary for temporal plugin

---
+++ packages/plugins/src/index.ts
@@ -0,0 +1,7 @@
+import { TemporalConditionResolver } from "./temporal.js";
+
+export { TemporalConditionResolver } from "./temporal.js";
+
+export type { TemporalRule, TemporalRoleMeta } from "./types.js";
+
+export const temporalPlugin = new TemporalConditionResolver();
