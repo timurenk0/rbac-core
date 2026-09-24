105f14d604b1a58f98630194c959493cb3ba8da5
timurenk0 <zheltenkovtimur@gmail.com>
2026-09-24 02:25:31 +0200

    Added plugin types

---
+++ packages/plugins/src/types.ts
@@ -0,0 +1,13 @@
+export interface TemporalRule {
+    readonly validFrom?: string;
+    readonly validUntil?: string;
+    readonly weekdays?: number[]; // 0-6 (Sun-Sat) JavaScript Date object based
+    readonly hours: {
+        readonly from: string;
+        readonly until: string;
+    }
+}
+
+export interface TemporalRoleMeta {
+    readonly temporal?: TemporalRule;
+}
